46e93775771135d8de0415685fa5e29332babf1e
Matthew Branning <48932625+juandap1@users.noreply.github.com>
2025-12-05 23:50:28 -0500

    BUN!!!!

---
+++ bun-backend/index.ts
@@ -0,0 +1,77 @@
+import { serve, S3Client } from "bun";
+
+const PORT = 6989;
+const client = new S3Client({
+  accessKeyId: process.env.MINIO_ROOT_USER,
+  secretAccessKey: process.env.MINIO_ROOT_PASSWORD,
+  bucket: "bran-bucket",
+  endpoint: `http://${process.env.MINIO_ENDPOINT}`,
+});
+
+function postProcessResponse(response: Response) {
+  response.headers.set("Access-Control-Allow-Origin", "*");
+  return response;
+}
+
+serve({
+  port: PORT,
+  async fetch(req) {
+    const path = new URL(req.url).pathname;
+    const method = req.method;
+
+    if (path === "/") {
+      return postProcessResponse(new Response("Hello World"));
+    }
+
+    if (path === "/upload" && method === "POST") {
+      const formData = await req.formData();
+      const file = formData.get("file");
+      if (!file || !(file instanceof File)) {
+        return postProcessResponse(
+          new Response("No file provided.", { status: 400 })
+        );
+      }
+      const fileName = file.name;
+      const destination = `pics/${fileName}`;
+      await client.write(destination, file);
+
+      console.log(`File uploaded successfully: ${destination}`);
+      return postProcessResponse(
+        new Response(`File uploaded successfully saved as ${destination}`, {
+          status: 200,
+        })
+      );
+    }
+
+    if (path === "/list-objects" && method === "GET") {
+      const objects = await client.list();
+      return postProcessResponse(
+        new Response(JSON.stringify(objects), { status: 200 })
+      );
+    }
+
+    if (path.startsWith("/object/") && method === "GET") {
+      const key = path.substring("/object/".length);
+      console.log(key);
+      if (!key) {
+        return postProcessResponse(
+          new Response("No key provided.", { status: 400 })
+        );
+      }
+      const object = client.file(key);
+      console.log(object);
+      console.log(object.size);
+      if (!object || object.size === 0) {
+        return postProcessResponse(
+          new Response("Object not found.", { status: 404 })
+        );
+      }
+      return postProcessResponse(new Response(object));
+    }
+
+    // 404s
+    return postProcessResponse(new Response("Page not found", { status: 404 }));
+  },
+});
+
+console.log(`Listening on http://localhost:${PORT} ...`);
